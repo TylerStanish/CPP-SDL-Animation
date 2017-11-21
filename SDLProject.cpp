caa48a6f9f44f079e5819ec1165bb522de42839e
Tyler Stanish <tystanish@gmail.com>
2017-11-20 21:12:20 -0500

    modified individual pixels and more with buffers

--- SDLProject.cpp
+++ SDLProject.cpp
@@ -1,11 +1,3 @@
-//============================================================================
-// Name        : SDLProject.cpp
-// Author      : 
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
-
 #include <iostream>
 #include <SDL.h>
 using namespace std;
@@ -33,6 +25,51 @@ int main() {
 		return 2;
 	}
 
+	// the last arg is responsible for refreshing from top-left corner to top-right, then next row, etc.
+	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC);
+	SDL_Texture *texture = SDL_CreateTexture(
+			renderer,
+			SDL_PIXELFORMAT_RGBA8888,
+			SDL_TEXTUREACCESS_STATIC,
+			SCREEN_WIDTH,
+			SCREEN_HEIGHT
+			);
+	if(renderer == NULL){
+		cout << "Could not create renderer" << endl;
+		SDL_DestroyWindow(window);
+		SDL_Quit();
+		return 3;
+	}
+	if(texture == NULL){
+		cout << "Could not create texture" << endl;
+		SDL_DestroyRenderer(renderer);
+		SDL_DestroyWindow(window);
+		SDL_Quit();
+		return 4;
+	}
+
+	// SDL defines this because an int may not be the same size on all operating systems
+	// also, a buffer is an area of memory
+	Uint32 *buffer = new Uint32[SCREEN_WIDTH * SCREEN_HEIGHT];
+	// memset allows you to set a block of memory with a particular value
+	// 0xFF is hexadecimal for 255
+	memset(buffer, 0, SCREEN_WIDTH*SCREEN_HEIGHT*sizeof(Uint32));
+
+	// setting an individual pixel
+	// setting all 4 bytes to FFs
+	buffer[30000] = 0xFFFFFFFF;
+
+	for(int i=0; i < SCREEN_WIDTH*SCREEN_HEIGHT; i++){
+		// this does same thing as memset, which is slower because for loop is slower
+		buffer[i] = 0xFF0000FF;
+	}
+
+	// the pitch (last arg)
+	SDL_UpdateTexture(texture, NULL, buffer, SCREEN_WIDTH * sizeof(Uint32));
+	SDL_RenderClear(renderer);
+	SDL_RenderCopy(renderer, texture, NULL, NULL);
+	SDL_RenderPresent(renderer);
+
 	bool quit = false;
 	SDL_Event event;
 	while(!quit){
@@ -47,6 +84,10 @@ int main() {
 		}
 	}
 
+	delete [] buffer;
+	// destroy texture first because it's using the renderer, just as a child class calls destructor first
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyTexture(texture);
 	SDL_DestroyWindow(window);
 	SDL_Quit();
 
